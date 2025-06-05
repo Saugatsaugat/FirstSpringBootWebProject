6c77d69169744a8c1be12d98edad808a14cf2f5a
Saugat Thapa <saugatthapa2073@gmail.com>
2025-06-05 18:40:55 -0500

    Spring boot web project for learning initiated and coded first hello project using embedded tomcat server and JSP with tomcat jasper

---
+++ src/main/java/com/saugat/firstspringbootwebproject/FirstSpringBootWebProjectApplication.java
@@ -0,0 +1,13 @@
+package com.saugat.firstspringbootwebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootWebProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,7 @@
+<%@page language="java" %>
+<html>
+<body>
+<h1>Hello World!!!</h1>
+<h3>My name is ?.</h3>
+</body>
+</html>
